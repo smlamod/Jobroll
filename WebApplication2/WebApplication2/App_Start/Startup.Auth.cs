29c91e98fea5bdd43e80d0d361601169d1b81f1a
smlamod <shawn.lamod@gmail.com>
2016-08-29 01:37:56 +0800

    Removed References, should build faster

--- WebApplication2/WebApplication2/App_Start/Startup.Auth.cs
+++ WebApplication2/WebApplication2/App_Start/Startup.Auth.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.DataProtection;
-using Microsoft.Owin.Security.Google;
 using Owin;
 using WebApplication2.Models;
 using Owin.Security.Providers.LinkedIn;
